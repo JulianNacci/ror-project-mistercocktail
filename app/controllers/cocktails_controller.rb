5939fda0de8ee80fe299c999d98221996d10c92e
Julian Nacci <julian.nacci@gmail.com>
2015-08-20 15:33:44 +0200

    design implemanted / controller and views cocktail ok

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,35 @@
+class CocktailsController < ApplicationController
+  before_action :find_cocktail, only: [:show, :destroy]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save ? (redirect_to cocktail_path(@cocktail)) : (render'new')
+  end
+
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+end
